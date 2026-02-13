a59103521ca7ad4142124f15f0abad4ce02e38c7
bank_npru <punsan.somkla@gmail.com>
2026-02-13 18:23:38 +0700

    feat: add Card component with customizable themes and slot support

--- src/volt/utils.ts
+++ src/volt/utils.ts
@@ -1,15 +1,9 @@
+import { twMerge } from 'tailwind-merge';
 import { mergeProps } from 'vue';
 
-const simpleMergeClass = (...values: any[]) =>
-  values
-    .flat()
-    .filter(Boolean)
-    .join(' ')
-    .trim()
-
 export const ptViewMerge = (globalPTProps = {} as any, selfPTProps = {} as any, datasets: any) => {
-  const { class: globalClass, ...globalRest } = globalPTProps
-  const { class: selfClass, ...selfRest } = selfPTProps
+    const { class: globalClass, ...globalRest } = globalPTProps;
+    const { class: selfClass, ...selfRest } = selfPTProps;
 
-  return mergeProps({ class: simpleMergeClass(globalClass, selfClass) }, globalRest, selfRest, datasets)
-}
+    return mergeProps({ class: twMerge(globalClass, selfClass) }, globalRest, selfRest, datasets);
+};
